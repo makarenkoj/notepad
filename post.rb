5acfe0931863a3f4644378a3f0e467e3370940e6
makarenkoj <makarenkoj@gmail.com>
2019-07-14 00:23:08 +0300

    initial commit

---
+++ post.rb
@@ -0,0 +1,31 @@
+class Post
+  def initialize
+    @created_at = Time.now
+    @text = nil
+  end
+
+  def read_from_console
+
+  end
+
+  def to_strings
+
+  end
+
+  def save
+    file = File.new(file_path, "w:UTF-8")
+
+    for item in to_strings do
+      file.puts(item)
+    end
+
+    file.close
+  end
+
+  def file_path
+    curent_path = File.dirname(__FILE__)
+
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+    return curent_path + "/" + file_name
+  end
+end
