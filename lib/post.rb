b197d39de66b1b9c975c92aca56de68e6a1cac29
makarenkoj <makarenkoj@gmail.com>
2019-08-18 00:19:23 +0300

    save data sql file

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,14 @@
+require 'sqlite3'
 
 class Post
-
+@@SQLITE_DB_FILE = 'notepad.sqlite'
   def self.post_types
-    [Memo, Task, Link]
+    {'Memo' => Memo, 'Task' => Task, 'Link' => Link}
   end
 
 
-  def self.create(type_index)
-    post_types[type_index].new
+  def self.create(type)
+    post_types[type].new
   end
 
   def initialize
@@ -37,5 +38,33 @@ class Post
     file_time = @created_at.strftime('%Y-%m-%d_%H-%M-%S')
 
     "#{current_path}/#{self.class.name}_#{file_time}.txt"
+  end
+
+  def save_to_db
+    db = SQLITE3::Database.open(@@SQLITE_DB_FILE)
+    db.result_as_hash = true
+
+    db.execute(
+          "INSERT INTO posts (" +
+          to_db_hash.keys.join(',') +
+              ")" +
+              " VALUES (" +
+              ('?,'*to_db_hash.keys.size).chomp(',') +
+              ")",
+          to_db_hash.values
+    )
+
+    insert_row_id = db.last_insert_row_id_
+
+    db.close
+
+    return insert_row_id
+  end
+
+  def to_db_hash
+    {
+        'type' => self.class.name,
+        'created_at' => @created_at.to_s
+    }
   end
 end
--- new_post.rb
+++ new_post.rb
@@ -19,7 +19,7 @@ puts 'Привет, я твой блокнот!'
 puts
 puts 'Что хотите записать в блокнот?'
 
-choices = Post.post_types
+choices = Post.post_types.keys
 
 choice = -1
 
@@ -31,10 +31,10 @@ until choice >= 0 && choice < choices.size
   choice = gets.to_i
 end
 
-entry = Post.create(choice)
+entry = Post.create(choices[choice])
 
 entry.read_from_console
 
-entry.save
+id = entry.save_to_db
 
-puts 'Ваша запись сохранена!'
+puts "Ваша запись сохранена, id = #{id}"
