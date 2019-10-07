d42bef46a766bf14bca965cb4ec00bdf48bdd9d1
makarenkoj <makarenkoj@gmail.com>
2019-10-08 01:05:23 +0300

    35-3/35-4

--- new_post.rb
+++ new_post.rb
@@ -1,4 +1,4 @@
-#Encoding:UTF-8
+# encoding: UTF-8
 
 if Gem.win_platform?
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
@@ -14,20 +14,18 @@ require_relative 'lib/memo'
 require_relative 'lib/link'
 require_relative 'lib/task'
 
-
 puts 'Привет, я твой блокнот!'
+puts 'Записываю новые записи в базу SQLite'
 puts
 puts 'Что хотите записать в блокнот?'
 
 choices = Post.post_types.keys
 
 choice = -1
-
 until choice >= 0 && choice < choices.size
   choices.each_with_index do |type, index|
     puts "\t#{index}. #{type}"
   end
-
   choice = gets.to_i
 end
 
@@ -37,4 +35,4 @@ entry.read_from_console
 
 rowid = entry.save_to_db
 
-puts "Ваша запись сохранена, id = #{rowid}"
+puts "Запись сохранена в базе, id = #{rowid}"
--- read.rb
+++ read.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# encoding: UTF-8
 
 if Gem.win_platform?
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
@@ -9,35 +9,36 @@ if Gem.win_platform?
   end
 end
 
-require_relative 'post'
-require_relative 'memo'
-require_relative 'link'
-require_relative 'task'
+require_relative 'lib/post'
+require_relative 'lib/memo'
+require_relative 'lib/link'
+require_relative 'lib/task'
 
 require 'optparse'
 
 options = {}
 
 OptionParser.new do |opt|
-   opt.banner = 'Usage: read.rb [options]'
+  opt.banner = 'Usage: read.rb [options]'
 
-   opt.on('-h', 'Prints this help') do
+  opt.on('-h', 'Prints this help') do
     puts opt
     exit
   end
 
   opt.on('--type POST_TYPE', 'какой тип постов показывать ' \
          '(по умолчанию любой)') { |o| options[:type] = o }
-
   opt.on('--id POST_ID', 'если задан id — показываем подробно ' \
          ' только этот пост') { |o| options[:id] = o }
-
   opt.on('--limit NUMBER', 'сколько последних постов показать ' \
          '(по умолчанию все)') { |o| options[:limit] = o }
-
 end.parse!
 
-result = Post.find(options[:limit], options[:type], options[:id])
+result = if options[:id].nil?
+           Post.find_all(options[:limit], options[:type])
+         else
+           Post.find_by_id(options[:id])
+         end
 
 if result.is_a? Post
   puts "Запись #{result.class.name}, id = #{options[:id]}"
@@ -52,14 +53,12 @@ else
   print '| @due_date          '
   print '|'
 
- result.each do |row|
+  result.each do |row|
     puts
 
     row.each do |element|
       element_text = "| #{element.to_s.delete("\n")[0..17]}"
-
       element_text << ' ' * (21 - element_text.size)
-
       print element_text
     end
 
